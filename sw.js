8d5725496db7f767939dba69ca181df90a206676
zhangliang <zhangliang32@jd.com>
2019-05-08 15:10:36 +0800

    change the test page.

--- sw.js
+++ sw.js
@@ -2,11 +2,9 @@ self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('v1').then(cache => {
       return cache.addAll([
-        '/dist/web/20170920/',
-        '/dist/web/js/',
-        '/dist/web/css/20170920/',
-        '/dist/web/images/',
-        '/dist/web/fonts/'
+        '/static/js/',
+        '/static/css/',
+        '/static/images/',
       ]);
     })
   );
