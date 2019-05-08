9f18608c8710a6e092d9db8cc562472b8b0a54b1
zhangliang <zhangliang32@jd.com>
2019-05-08 15:22:35 +0800

    edit the sw.js and add jquery

--- sw.js
+++ sw.js
@@ -2,9 +2,10 @@ self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('v1').then(cache => {
       return cache.addAll([
-        '/static/js/',
-        '/static/css/',
-        '/static/images/',
+        '/static/js/antd.js',
+        '/static/js/jquery-3.1.1.min.js',
+        '/static/css/index.css',
+        '/static/images/dbc88cfeintro-img-2.jpg',
       ]);
     })
   );
