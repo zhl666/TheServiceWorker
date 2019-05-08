b20ae2cbe281b90df142910e171c1d759ed9ce68
zhangliang <zhangliang32@jd.com>
2019-05-08 15:26:47 +0800

    delete the antd files.

--- sw.js
+++ sw.js
@@ -2,7 +2,6 @@ self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('v1').then(cache => {
       return cache.addAll([
-        '/static/js/antd.js',
         '/static/js/jquery-3.1.1.min.js',
         '/static/css/index.css',
         '/static/images/dbc88cfeintro-img-2.jpg',
