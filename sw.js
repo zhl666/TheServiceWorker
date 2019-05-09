a0222262183894273caced532584ff02fa5ea5c9
zhangliang <zhangliang32@jd.com>
2019-05-09 10:23:14 +0800

    edit the service worker files path.

--- sw.js
+++ sw.js
@@ -2,9 +2,9 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('v1').then(cache => {
       return cache.addAll([
-        '/static/js/jquery-3.1.1.min.js',
-        '/static/css/index.css',
-        '/static/images/dbc88cfeintro-img-2.jpg',
+        '/serviceworker/static/js/jquery-3.1.1.min.js',
+        '/serviceworker/static/css/index.css',
+        '/serviceworker/static/images/dbc88cfeintro-img-2.jpg',
       ]);
     })
   );
